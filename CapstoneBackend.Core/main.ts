2f6c3653575fc9025413590d5a33d5eed6a079ff
graceson <gwheatley@ftstudent.org>
2025-11-19 16:55:50 -0500

    😭

---
+++ CapstoneBackend.Core/main.ts
@@ -0,0 +1,32 @@
+﻿// TODO decide on TS or C#...
+// Mr. Lemke please spare me but I'd probably do better with Node.JS than C# since I already have a ton of experience with JS. 
+
+//TS test to see if it works on my system :P
+
+
+
+import { createPool, PoolOptions} from 'mysql2';
+
+
+
+const access : PoolOptions = {
+    host: 'localhost',
+    user: 'root',
+    password: 'PoofBall#1', //Don't do this in actual api, ja.
+    database: 'fun_facts_db',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+    
+};
+
+export const connection = createPool(access);
+
+connection.getConnection((err, conn) => {
+    if (err) {
+        console.error('Error you idiot!');
+        return;
+    }
+    console.log('It worked!');
+    conn.release();
+})
